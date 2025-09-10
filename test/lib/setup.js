1c4aed1c47c5d2d7b25aca8db342dfd36aaaa4b1
Muthu Arumugam <muthu.kumar@me.com>
2025-09-10 13:44:40 -0400

    Refactor test setup by standardizing import statements for chai, chai-as-promised, sinon-chai, and sinon to use single quotes, and update global variable assignments for consistency.

--- test/lib/setup.js
+++ test/lib/setup.js
@@ -1,10 +1,10 @@
-const chai = require("chai");
-const chaiAsPromised = require("chai-as-promised").default;
-const sinonChai = require("sinon-chai").default;
-const sinon = require("sinon");
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised').default
+const sinonChai = require('sinon-chai').default
+const sinon = require('sinon')
 
-chai.use(chaiAsPromised);
-chai.use(sinonChai);
+chai.use(chaiAsPromised)
+chai.use(sinonChai)
 
-global.expect = chai.expect;
-global.sinon = sinon;
+global.expect = chai.expect
+global.sinon = sinon
